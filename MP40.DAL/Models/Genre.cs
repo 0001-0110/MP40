9945f7d889d7f0686596e24acc6d45e431c4c807
Rémi TROTEL <remi.trotel@epita.fr>
2023-05-22 13:24:37 +0200

    Scaffolded models and database context

---
+++ MP40.DAL/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿namespace MP40.DAL.Models;
+
+public partial class Genre
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public virtual ICollection<Video> Videos { get; set; } = new List<Video>();
+}
---
+++ MP40.DAL/Models/Image.cs
@@ -0,0 +1,10 @@
+﻿namespace MP40.DAL.Models;
+
+public partial class Image
+{
+    public int Id { get; set; }
+
+    public string Content { get; set; } = null!;
+
+    public virtual ICollection<Video> Videos { get; set; } = new List<Video>();
+}
