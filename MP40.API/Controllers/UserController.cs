d719d6d500c356f79f4949ecc6de98569303cd70
Rémi TROTEL <remi.trotel@epita.fr>
2023-06-28 15:57:27 +0200

    Added User API endpoint

---
+++ MP40.API/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using MP40.BLL.Models;
+using MP40.BLL.Services;
+
+namespace MP40.Controllers
+{
+    public class UserController : ModelController<User>
+    {
+        public UserController(ILogger<ModelController<User>> logger, IDataService dataService) : base(logger, dataService) { }
+    }
+}
